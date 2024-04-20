10296dc8d0be51bb0b3cfabfa31f310b7a4c60ea
Aryan <aryanbandbe@gmail.com>
2024-04-21 00:34:33 +0530

    first commit

---
+++ ticketsys/info.py
@@ -0,0 +1,7 @@
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+DEFAULT_FROM_EMAIL = 'luffyandnami12@gmail.com'
+EMAIL_HOST_USER = 'luffyandnami12@gmail.com'
+EMAIL_HOST_PASSWORD='luffy@786'
+EMAIL_PORT = 587
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
